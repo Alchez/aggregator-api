74ee094b02b9dad4376d714d68f134de341334ce
Revant Nandgaonkar <revant.one@gmail.com>
2019-01-29 17:12:17 +0530

    update: root and apps
    update root dependencies
    update app dependencies
    
    use latest dependencies

--- scripts/build-docs.sh
+++ scripts/build-docs.sh
@@ -5,9 +5,14 @@ echo "Building Aggregator Service Docs"
     packages/aggregator-service/src \
     --name "Aggregator Server"
 
-echo "Building Aggregator Console Docs"
+echo "Building Aggregator Console Server Docs"
 ./node_modules/.bin/typedoc \
-    --out public/api/aggregator-console \
-    packages/aggregator-console/src \
+    --out public/api/aggregator-console-server \
     packages/aggregator-console/server \
-    --name "Aggregator Console"
+    --name "Aggregator Console Server"
+
+echo "Building Aggregator Console Client Docs"
+./node_modules/.bin/typedoc \
+    --out public/api/aggregator-console-client \
+    packages/aggregator-console/src \
+    --name "Aggregator Console Client"
